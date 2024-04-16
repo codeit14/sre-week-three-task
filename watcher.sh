8835f11c93e41a46dac98452144563facb8fce39
Vaibhav Jain <kaps12271@gmail.com>
2024-04-16 17:50:09 +0000

    added watcher

--- watcher.sh
+++ watcher.sh
@@ -1,1 +1,17 @@
-#TODO
+#!/bin/bash          
+NAMESPACE="sre"
+DEPLOYMENT="swype-app"
+RESTARTS=4
+while true
+do
+  COUNT=$(kubectl get pods -n ${NAMESPACE} -l app=${DEPLOYMENT} -o jsonpath="{.items[0].status.containerStatuses[0].restartCount}")
+
+  echo "Restarts: ${COUNT}"
+
+  if (( COUNT > RESTARTS )); then
+    kubectl scale --replicas=0 -n ${NAMESPACE} deployment/${DEPLOYMENT} 
+    break
+  fi
+
+  sleep 60
+done
